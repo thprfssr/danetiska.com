fe8bc5eee47d9e37f6c13f8a34ff9592b1fa9aa2
Javier Castro <jaca2899@gmail.com>
2024-08-22 21:54:52 -0400

    Corrected calendar.js script

--- calendar.js
+++ calendar.js
@@ -370,12 +370,12 @@ window.onload = function() {
 }
 
 document.getElementById('form_calendar_calculator').addEventListener('submit', function(event) {
-	//event.preventDefault();
+	event.preventDefault();
 
 	var date_string = document.getElementById('input_date').value
 	var calendar_type = document.getElementById('select_calendar').value
 
-	var y, m, d = interpret_date_string(date_string)
+	var [y, m, d] = interpret_date_string(date_string)
 
 	var n = 0
 	if (calendar_type == "g") {
@@ -388,14 +388,14 @@ document.getElementById('form_calendar_calculator').addEventListener('submit', f
 		n = 0
 	}
 
-	var yg, mg, dg = gregorian_from_julian_day(n)
-	var yj, mj, dj = julian_from_julian_day(n)
-	var yd, md, dd = danetian_from_julian_day(n)
+	var [yg, mg, dg] = gregorian_from_julian_day(n)
+	var [yj, mj, dj] = julian_from_julian_day(n)
+	var [yd, md, dd] = danetian_from_julian_day(n)
 	var date_g = iso_format(yg, mg, dg)
 	var date_j = iso_format(yj, mj, dj)
 	var date_d = iso_format(yd, md, dd)
 
-	document.getElementById('result_gregorian').innerText = 'Gregorian: ${date_g}'
-	document.getElementById('result_julian').innerText = 'Julian: ${date_j}'
-	document.getElementById('result_danetian').innerText = 'Danetian: ${date_d}'
+	document.getElementById('result_gregorian').innerText = `Gregorian: ${date_g}`
+	document.getElementById('result_julian').innerText = `Julian: ${date_j}`
+	document.getElementById('result_danetian').innerText = `Danetian: ${date_d}`
 })
