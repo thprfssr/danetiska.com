5769c53a62f0661a2f52bf68d7a24d4655ca7f24
Javier Castro <jaca2899@gmail.com>
2025-04-08 21:03:30 -0400

    Published blog post 'Factorial Fun'.

--- assets/scripts/mathjax-config.js
+++ assets/scripts/mathjax-config.js
@@ -1,14 +1,17 @@
 window.MathJax = {
+  loader: {load: ['[tex]/physics', '[tex]/cancel']},
   tex: {
     inlineMath: [['$', '$'], ['\\(', '\\)']],
     displayMath: [['$$', '$$'], ['\\[', '\\]']],
     processEscapes: true,
-    macros: {
+    packages: ['base', 'ams', 'physics', 'cancel']
+    /*macros: {
       d: "\\mathrm{d}",
       dd: ["\\frac{\\mathrm{d}#1}{\\mathrm{d}#2}", 2],
       pd: "\\partial",
       pp: ["\\frac{\\partial #1}{\\partial #2}", 2]
     }
+    */
   },
   options: {
     skipHtmlTags: ['script', 'noscript', 'style', 'textarea', 'pre', 'code'],
