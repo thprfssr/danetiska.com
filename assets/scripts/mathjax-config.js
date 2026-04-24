db4a360cd56943825ca19acdf3597bdf2f7f610a
Javier Castro <javier@danetiska.com>
2026-04-23 22:13:57 -0400

    Updated Mathjax3 --> Mathjax4

--- assets/scripts/mathjax-config.js
+++ assets/scripts/mathjax-config.js
@@ -1,21 +1,14 @@
 window.MathJax = {
-  loader: {load: ['[tex]/physics', '[tex]/cancel']},
+  loader: {load: ['[tex]/physics', '[tex]/ams', '[tex]/cancel']},
   tex: {
-    inlineMath: [['$', '$'], ['\\(', '\\)']],
-    displayMath: [['$$', '$$'], ['\\[', '\\]']],
+    inlineMath: {'[+]': [['$', '$']]},
+    displayMath: {'[+]': [['$$', '$$']]},
     processEscapes: true,
-    packages: ['base', 'ams', 'physics', 'cancel', 'require']
-    /*macros: {
-      d: "\\mathrm{d}",
-      dd: ["\\frac{\\mathrm{d}#1}{\\mathrm{d}#2}", 2],
-      pd: "\\partial",
-      pp: ["\\frac{\\partial #1}{\\partial #2}", 2]
-    }
-    */
   },
+  /*
   options: {
     skipHtmlTags: ['script', 'noscript', 'style', 'textarea', 'pre', 'code'],
     ignoreHtmlClass: 'no-mathjax',
   }
+  */
 };
-
